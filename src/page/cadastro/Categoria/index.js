b0a34a82af716e71fde942dff176950c2b49fcba
Bruno Torres <brunogtorres@hotmail.com>
2020-08-02 03:11:41 -0300

    voltando

--- src/page/cadastro/Categoria/index.js
+++ src/page/cadastro/Categoria/index.js
@@ -6,7 +6,6 @@ import Button from '../../../components/Button';
 import useForm from '../../../hocks/useForm';
 import categoriasRepository from '../../../repositories/categorias';
 import styled from "styled-components";
-import { ContentTable, TableContainer, } from './styles';
 
 const Content = styled.p`
 text-decoration: none;
@@ -112,56 +111,21 @@ function CadastroCategoria(){
         </Button>
       </form>
 
-      <TableContainer>
-            <ContentTable>
-              {categorias[0] === undefined ? (
-                <tbody>
-                  <tr>
-                    <td>Não existe categoria cadastrada!</td>
-                  </tr>
-                </tbody>
-              ) : (
-                <>
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>Descrição</th>
-                      <th>Editar</th>
-                      <th>Remover</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {categorias.map((categoriaItem, index) => (
-                      <tr key={`${categoriaItem.titulo}${index}`}>
-                        <td>{categoriaItem.titulo}</td>
-                        <td>{categoriaItem.descricao}</td>
-                        <td>
-                          <Button
-                            variant="contained"
-                            color="primary"
-                            
-                            
-                          >
-                            Editar
-                          </Button>
-                        </td>
-                        <td>
-                          <Button
-                            variant="contained"
-                            color="secondary"
-                            
-                            
-                          >
-                            Remover
-                          </Button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </>
-              )}
-            </ContentTable>
-          </TableContainer>
+      {categorias.length === 0 && (
+        <div>
+          {/* Cargando... */}
+          Loading...
+        </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
+   
       <Content>
         <Link to='/' style={{ textDecoration: 'none' }}>
             <p>← Ir para Home</p>
