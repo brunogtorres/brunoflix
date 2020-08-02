45685a1d34778da891aba5a94762fbf18df0a2a1
Bruno Torres <brunogtorres@hotmail.com>
2020-08-02 00:56:13 -0300

    tentativa

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -3,7 +3,7 @@ import config from '../config';
 const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
 function getAll() {
-  return fetch(`${URL_CATEGORIES}`)
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (respostaDoServidor) => {
       if (respostaDoServidor.ok) {
         const resposta = await respostaDoServidor.json();
